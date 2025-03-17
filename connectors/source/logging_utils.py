f9b734347d52d4552295e28304440555f75441a3
tbs17 <jia.t.shen@gmail.com>
2025-03-17 17:02:50 -0400

    fix config access error

--- connectors/source/logging_utils.py
+++ connectors/source/logging_utils.py
@@ -1,15 +1,18 @@
 from typing import Literal
 
 from unstructured_client.models.shared import (
-    SourceConnectorInformation, AzureSourceConnectorConfig, SourceConnectorInformationConfig
+    SourceConnectorInformation,
+    SourceConnectorInformationConfig,
 )
 
 
-def create_log_for_created_updated_source_connector(response, source_name: str,
-                                                    created_or_updated: Literal[
-                                                 'Created', "Updated"]) -> str:
+def create_log_for_created_updated_source_connector(
+    response,
+    source_name: str,
+    created_or_updated: Literal["Created", "Updated"],
+) -> str:
     info: SourceConnectorInformation | None = response.source_connector_information
-    config: SourceConnectorInformationConfig | None = response.config if info else None
+    config: SourceConnectorInformationConfig | None = info.config if info else None
 
     result = [f"{source_name} Source Connector {created_or_updated}:"]
 
@@ -17,8 +20,13 @@ def create_log_for_created_updated_source_connector(response, source_name: str,
         result.extend([f"Name: {info.name}", f"ID: {info.id}"])
 
     if config:
-        result.extend(["Configuration:", '  remote_url: {config.remote_url}',
-                       '  recursive: {config.recursive}'])
+        result.extend(
+            [
+                "Configuration:",
+                "  remote_url: {config.remote_url}",
+                "  recursive: {config.recursive}",
+            ],
+        )
 
     combined_result = "\n".join(result)
     return combined_result
