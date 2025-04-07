af481ba1a685440ab0147176bb9d80a74a407de4
Kamil Plucinski <kamil.plucinski@deepsense.ai>
2025-04-07 12:51:46 +0200

    Refactor imports

--- uns_mcp/connectors/destination/__init__.py
+++ uns_mcp/connectors/destination/__init__.py
@@ -1,34 +1,14 @@
 from mcp.server.fastmcp import FastMCP
 
-from connectors.destination.astra import delete_astradb_destination
-from connectors.destination.databricks_vdt import (
-    delete_databricks_delta_table_destination,
-)
-from connectors.destination.databricksvolumes import (
-    delete_databricks_volumes_destination,
-)
-from connectors.destination.destination_tool import (
+from uns_mcp.connectors.destination.destination_tool import (
     create_destination_connector,
     update_destination_connector,
 )
-from connectors.destination.mongo import (
-    delete_mongodb_destination,
-)
-from connectors.destination.neo4j import delete_neo4j_destination
-from connectors.destination.pinecone import delete_pinecone_destination
-from connectors.destination.s3 import delete_s3_destination
-from connectors.destination.weaviate import delete_weaviate_destination
+from uns_mcp.connectors.destination.generic import delete_destination
 
 
 def register_destination_connectors(mcp: FastMCP):
     """Register all destination connector tools with the MCP server."""
     mcp.tool()(create_destination_connector)
     mcp.tool()(update_destination_connector)
-    mcp.tool()(delete_s3_destination)
-    mcp.tool()(delete_weaviate_destination)
-    mcp.tool()(delete_astradb_destination)
-    mcp.tool()(delete_neo4j_destination)
-    mcp.tool()(delete_mongodb_destination)
-    mcp.tool()(delete_databricks_volumes_destination)
-    mcp.tool()(delete_databricks_delta_table_destination)
-    mcp.tool()(delete_pinecone_destination)
+    mcp.tool()(delete_destination)
