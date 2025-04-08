f1af78d99c6cee9e46cfb2804c8c6fcb032a69e8
Kamil Plucinski <kamil.plucinski@deepsense.ai>
2025-04-08 14:23:36 +0200

    Bump update too

--- uns_mcp/connectors/destination/weaviate.py
+++ uns_mcp/connectors/destination/weaviate.py
@@ -90,6 +90,11 @@ async def update_weaviate_destination(
         cluster_url (optional): URL of the weaviate cluster
         collection (optional): Name of the collection(like a file) to use in the weaviate cluster
 
+    Note: The collection is a table in the Weaviate cluster. In the platform, the collection
+        name can be generated automatically; however, this is not yet supported via the API.
+        Therefore, the collection name is required. Please note that the schema cannot be empty
+        and must contain at least a record_id: Text.
+
     Returns:
         String containing the updated destination connector information
     """
